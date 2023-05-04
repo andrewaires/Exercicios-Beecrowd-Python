ed69e4aed64707a196e72a53abb94e0a1c3b726d
Andrew Aires <121694498+andrewaires@users.noreply.github.com>
2023-05-03 21:18:26 -0300

    Add files via upload

---
+++ 01-Iniciante/2757 - Entrada e Saída de Números Inteiros.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(f'A = {a}, B = {b}, C = {c}')
+print(f'A = {a:10d}, B = {b:10d}, C = {c:10d}')
+print(f'A = {str(a).zfill(10)}, B = {str(b).zfill(10)}, C = {str(c).zfill(10)}')
+print(f'A = {a:<10d}, B = {b:<10d}, C = {c:<10d}')
