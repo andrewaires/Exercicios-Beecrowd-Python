8e18d6d34a128c11dabad0e65158236369783b51
Andrew Aires <121694498+andrewaires@users.noreply.github.com>
2023-05-06 17:20:12 -0300

    Add files via upload

--- 01-Iniciante/2632 - Magic and Sword.py
+++ 01-Iniciante/2632 - Magic and Sword.py
@@ -26,15 +26,15 @@ elementos = {
 }
 
 def intercessao(x1, y1, raio, x0, y0, largura, altura):
-    cDx = abs(x1 - (x0 + largura / 2))
-    cDy = abs(y1 - (y0 + altura / 2))
+    diferenca_x = abs(x1 - (x0 + largura / 2))
+    diferenca_y = abs(y1 - (y0 + altura / 2))
 
-    if (cDx > (largura / 2 + raio)) or (cDy > (altura / 2 + raio)):
+    if (diferenca_x > (largura / 2 + raio)) or (diferenca_y > (altura / 2 + raio)):
         return False
-    elif (cDx <= largura / 2) or (cDy <= altura / 2):
+    elif (diferenca_x <= largura / 2) or (diferenca_y <= altura / 2):
         return True
     else:
-        return ((cDx - largura/2)**2 + (cDy - altura/2)**2) <= raio**2
+        return ((diferenca_x - largura/2)**2 + (diferenca_y - altura/2)**2) <= raio**2
 
 
 t = int(input())
